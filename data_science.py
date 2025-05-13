d594b9e2f05d7c750d3bb085a208ba11b7f65630
Maraalll <118250379+Maraalll@users.noreply.github.com>
2025-05-13 12:57:25 +0500

    Update data_science.py

--- data_science.py
+++ data_science.py
@@ -4,6 +4,28 @@ import data_science_2
 import data_science_3
 st.set_page_config(page_title="Платформа для поиска работы", page_icon="💼", layout="wide")
 
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        background-color: #1f77b4 !important;  /* синий цвет */
+        color: white !important;
+        font-weight: bold;
+        padding: 10px 20px;
+        border-radius: 12px;
+        border: none;
+        transition: background-color 0.3s ease;
+    }
+
+    .stButton>button:hover {
+        background-color: #135a96 !important;
+        color: white !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.sidebar.title("📌 Навигация")
 page = st.sidebar.radio("📄 Выберите страницу:", [
     "🔎 Поиск вакансий",
