6c51a80530c5fcea62a196578a4718354c866277
Maraalll <118250379+Maraalll@users.noreply.github.com>
2025-12-18 02:00:54 +0500

    Update data_science.py

--- data_science.py
+++ data_science.py
@@ -77,24 +77,6 @@ elif page == "📝 Генератор резюме":
 elif page == "🌟 Оценка компании":
     data_science_2.page_rate_company()
 elif page == "🌍 CityFit AI":
-    st.markdown(
-        """
-        <div style="padding-bottom:20px;">
-            <h2>🌍 CityFit AI</h2>
-            <p style="color:gray; font-size:16px;">
-                Интеллектуальный ML-модуль, который показывает,
-                <b>в каком городе твой шанс трудоустройства выше</b>,
-                на основе анализа рынка вакансий
-            </p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
-    # вызов ML-функции
-    data_science_4.cityfit_ai(
-        st.session_state.get("user_profile", {}).get("city")
-    )
-
+    data_science_4.page_cityfit_ai()
 elif page == "🎯 Skill Match/Fit":
     data5.page_skill_match()
